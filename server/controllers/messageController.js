434e6c4a988cb2decbe9c7541bf605fd6a1c3450
Gaurav-Attri <attrigaurav60@gmail.com>
2025-10-17 09:29:15 +0530

    Created Message model and getUsersForSidebar, getMessage and markMessageAsSeen controller functions

---
+++ server/controllers/messageController.js
@@ -0,0 +1,60 @@
+import User from "../models/User.js";
+import Message from "../models/Message.js";
+
+// Retreiving all users except the logged-in user to show in the sidebar
+export const getUsersForSidebar = async (req, res) => {
+    try{
+        const userId = req.user._id;
+        const filteredUsers = await User.find({_id: {$ne: userId}}).select("-password");
+
+        // Counting number of unseen messages from each user
+        const unseenMessages = {};
+        const promises = filteredUsers.map(async (user) => {
+            const messages = await Message.find({senderId: user._id, receiverId: userId, seen: false})
+            if(messages.length > 0){
+                unseenMessages[user._id] = messages.length;
+            }
+        });
+
+        await Promise.all(promises);
+        res.json({success: true, users: filteredUsers, unseenMessages});
+    }
+    catch(error){
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Retreiving all messages from the selected user
+export const getMessage = async (req, res) => {
+    try{
+        const {id: selectedUserId} = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: selectedUserId},
+                {senderId: selectedUserId, receiverId: myId}
+            ]
+        });
+        await Message.updateMany({senderId: selectedUserId, receiverId: myId}, {seen: true});
+        res.json({success: true, messages});
+    }
+    catch(error){
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Marking a message with given id as seen
+export const markMessagesAsSeen = async (req, res) => {
+    try{
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id, {seen: true});
+        res.json({success: true});
+    }
+    catch(error){
+        console.log(error.message);
+        res.json({success: false, message: error.message}); 
+    }
+}
