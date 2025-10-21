5decb1b6ff8662235e6be43de7b8a3e144482542
Gaurav-Attri <attrigaurav60@gmail.com>
2025-10-21 13:19:11 +0530

    Fixed DB connection function error

--- server/lib/db.js
+++ server/lib/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
     try{
         mongoose.connection.on("connected", () => console.log("MongoDB connected"));
-        await mongoose.connect(`${process.env.MONGO_URI}/quick-chat`)
+        await mongoose.connect(`${process.env.MONGO_URI}quick_chat`)
     } 
     catch(error){
         console.error(error);
